26ff5625a6fc34ce76ef82818f7de4cfc2ebbdf0
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-25 11:57:30 +0000

    Type_Converstion

---
+++ 01_basic/type_Converstion.js
@@ -0,0 +1,40 @@
+// let score="20abc"
+// let score=null
+// let score=true
+let score=undefined
+
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+;
+
+/*
+  "33" - 33
+  "33ba" - nan
+  true - 1; false - 0
+
+*/
+
+
+// let isLoggedIn=1
+// let isLoggedIn="hi"
+let isLoggedIn=""
+
+
+let boolean=Boolean(isLoggedIn)
+console.log(boolean);
+
+
+/*
+ 1 - true
+ 0 - false
+ "" - false
+ "hi" - true
+
+*/
+
