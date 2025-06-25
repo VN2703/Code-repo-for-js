d901d6844c723ee696140ca1b5c8a8da2ab8364c
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-25 11:56:40 +0000

    Datatypes

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat a;; js code as newer version
+
+// alert(3+2) // we are using nodejs, not browser so uts will nit give ans as 6
+let name="hitest"
+let age=35
+let isLoggedIn=false
+let state;
+
+//number =>2 to power 53
+// bigint
+//string =>""
+//boolean => true/false
+//null => standalone value , it tells that the value is empty
+//undefined => when value is given
+//symbol =>unique
+
+
+
+
+//object
+
+console.log( typeof null);  //object
+console.log( typeof undefined) //undefined
+
