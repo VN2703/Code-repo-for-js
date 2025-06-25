945e5838fab1019342fd2c9d0ef5e34ffe9f470c
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-25 07:24:30 +0000

    var, const, let ke kahani

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,56 @@
+
+
+const accId=1223
+let accEmail="vaish@google.com"
+var accPass="1234"
+accCity="jaipur"
+
+//accId=2 not allowed
+
+// console.log(accId);
+
+accEmail="raj@gmail.com"
+accPass="3455"
+accCity="mumbai"
+// console.table([accId,accEmail, accPass, accCity])
+
+/*
+      var: var can re assigned or re declared both
+      let: let can re assigned but can not declared 
+*/
+//var :
+// reassignment  
+var x=10
+console.log(x);
+ x="maya"
+ console.log(x);
+
+ //redeclare
+var x=10
+console.log(x);
+ x="maya"
+//  console.log(x);
+
+
+
+
+//  let :
+reassignment  
+let x=10
+console.log(x);
+ x="maya"
+ console.log(x);
+
+
+
+ //redeclare
+// let x=10
+// console.log(x);
+//      x="maya"
+//  console.log(x);
+ 
+
+
+
+
+
