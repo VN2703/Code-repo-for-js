0142817a60cbf6f2bd354547495cdf4fa180283e
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-25 11:56:59 +0000

    Operations

---
+++ 01_basic/operations.js
@@ -0,0 +1,27 @@
+let value=3
+let negValue= -value
+//console.log(negValue);
+   
+
+let s1="hi"
+let s2=" vish"
+ let s3=s1+s2
+ //console.log(s3);
+
+
+
+//  console.log("1"+2);
+//   console.log(1+'2');
+//    console.log("1"+2+2);
+//     console.log(1+2+"2");
+    
+console.log(+true); //1
+ console.log(+"");
+
+ let score=2
+ score++
+ 
+ console.log(score);
+ 
+
+ 
