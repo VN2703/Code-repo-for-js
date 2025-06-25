43f795df6fb7069d87ba3fc817833f8b4ba1b44f
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-25 12:14:17 +0000

    DataType_Summary

---
+++ 01_basic/summary_dataTypes.js
@@ -0,0 +1,34 @@
+// dataTypes= 2 types
+
+// 1. Primitive
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score=100;
+const scoreVal=10.3
+const temp=false
+let Email;
+
+const id=Symbol('123');
+
+const anotherId=Symbol('123')
+console.log(id===anotherId);  //false
+const bigNumber=657684548884544n
+
+
+
+// Refrence(Non Primitive)
+
+// Array, Objects, Functions
+
+const herso=["SRK", "Shreedevi", "kajul"];
+
+
+let myObj={
+    name:"vaish",
+    age: 22,
+}
+
+const myFunction=function(){
+    console.log("heelo");
+    
+}
