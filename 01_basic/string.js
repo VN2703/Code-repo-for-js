5e33b347713d4102b23eb83e8ae1c2e6ddc72eb0
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-26 09:28:33 +0000

    String

---
+++ 01_basic/string.js
@@ -0,0 +1,25 @@
+const name="nema"
+const repocnt= 4
+
+//console.log(name  + repocnt+" value");
+console.log(`mu name is ${name} my repo is ${repocnt}`);
+const gameName=new String('BLUE-WHALE')
+
+// console.log(gameName[0]);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+
+console.log(gameName.substring(0,5)); // will ignore -ve value
+console.log(gameName.slice(-10,4)); // will not ignore -ve value
+
+const newStr="  hitesh  "
+console.log(newStr);
+console.log(newStr.trim());
+
+
+
+
+
+
+
