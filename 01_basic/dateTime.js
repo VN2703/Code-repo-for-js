ff410c074eff6221efa134c32d35c50124840d16
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-26 09:28:12 +0000

    DateTime

---
+++ 01_basic/dateTime.js
@@ -0,0 +1,16 @@
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+
+// let myCreate=new Date(2023, 11, 25, 5,3)
+// console.log(myCreate.toLocaleString());
+
+let myTime=Date.now()
+console.log(myTime);
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
