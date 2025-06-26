d89a17fd9324d7495d3639e8d9e67f65bcf9932f
Vaishnavi Nema <163845840+VN2703@users.noreply.github.com>
2025-06-26 09:28:23 +0000

    Memory

---
+++ 01_basic/memory.js
@@ -0,0 +1,22 @@
+// Stack And Heap Memory :
+
+let myYoutube="vaish.com"
+let anotherName=myYoutube
+
+anotherName="chaiOrCode"
+
+console.log(myYoutube);
+console.log(anotherName);
+
+let userone = {
+    email:"@gmail.com",
+    upi: "user@upi"
+}
+let user2=userone
+user2.email="hitesh@gmail.com"
+
+console.log(userone.email);
+console.log(user2.email);
+
+
+
